da848bd91479edb1a37c6bfac87b96ef6bf0b4fb
Munesh Saini <sainimunesh124@gmail.com>
2020-01-08 23:10:07 +0530

    Add static file

--- demo_project/urls.py
+++ demo_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('demo_app.urls')),
+    path('api/v1/',include('demo_app.urls')),
 ]
